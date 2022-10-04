b15c7517869ca9ee6fc068f6d8ec2ed96d1a73cf
Flávio Eduardo <xflax00@gmail.com>
2022-10-04 16:34:05 -0300

    Func - Ver Saldo

--- src/App.java
+++ src/App.java
@@ -64,15 +64,15 @@ public class App {
 
         long convertor_num = Long.parseLong(numero_p);
         long convertor_cep = Long.parseLong(Cep_p);
-        // long convertor_cpf = Long.parseLong(CPF_p);
 
         Endereco endereco = new Endereco(logradouro_p, convertor_num, bairro_p, cidade_p, convertor_cep);
         Conta c1 = new Conta(numConta);
-        Cliente cliente_1 = new Cliente(Nome_p, CPF_p, endereco, c1, senha);
+        Cliente cliente = new Cliente(Nome_p, CPF_p, endereco, c1, senha);
 
-        clientes.add(cliente_1);
+        clientes.add(cliente);
 
         JOptionPane.showMessageDialog(null, "Número da conta: " + numConta);
+        // System.out.println(clientes.size());
     }
 
     public static void entra() {
@@ -82,74 +82,73 @@ public class App {
             String userCPF = JOptionPane.showInputDialog("Digite seu CPF: ");
             String userSenha = JOptionPane.showInputDialog("Digite sua senha: ");
 
-            if(userCPF.equals(clientes.get(0).getCPF()) && userSenha.equals(clientes.get(0).getSenha())){
+            // Percorrendo clientes
+            for(Cliente cliente : clientes){
+                // Autenticando cliente
+                if(userCPF.equals(cliente.getCPF()) && userSenha.equals(cliente.getSenha())){
 
-                // System.out.println(("Entrou"));
+                    JOptionPane.showMessageDialog(null, "Nome: " + cliente.getNome() + "\nNúmero de contas: " + cliente.getContas().size());
 
-                List<Integer> menu = new ArrayList<>();
-                menu.add(1);
-                menu.add(2);
+                    // Criando menu pós-login
+                    List<Integer> menu = new ArrayList<>();
+                    menu.add(1);
+                    menu.add(2);
 
-                Object[] menuEntrar = menu.toArray();
-                int opcSelecionada = JOptionPane.showOptionDialog(null, "1. Entrar em conta existente \n2. Criar nova conta", "Conta", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuEntrar, null);
+                    Object[] menuEntrar = menu.toArray();
+                    int opcSelecionada = JOptionPane.showOptionDialog(null, "1. Entrar em conta existente \n2. Criar nova conta", "Conta", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, menuEntrar, null);
 
-                if(opcSelecionada == 0){
-
-                    System.out.println("Número da conta: " + ((Conta) clientes.get(0).getContas().get(0)).getNumeroConta() + "\nSaldo: " + ((Conta) clientes.get(0).getContas().get(0)).getSaldo());
-
-                    List<Integer> opcoes = new ArrayList<>();
-
-                    for(Conta conta : clientes.get(0).getContas()){
-                        opcoes.add(conta.getNumeroConta());
-                    }
-
-                    Object[] opcoesConta = opcoes.toArray();
-                    int contaSelecionada = JOptionPane.showOptionDialog(null, "Selecione a conta", "Contas", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoesConta, null);
-
-                    int num_Conta = opcoes.get(contaSelecionada);
-
-                    System.out.println(num_Conta);
-
-                    for(Conta conta : clientes.get(0).getContas()){
-                        if(conta.getNumeroConta() == num_Conta){
-                            
+                    if(opcSelecionada == 0){
+    
+                        List<Integer> opcoes = new ArrayList<>();
+                        
+                        for(Conta conta : cliente.getContas()){
+                            opcoes.add(conta.getNumeroConta());
                         }
-                    }
+    
+                        Object[] opcoesConta = opcoes.toArray();
+                        int contaSelecionada = JOptionPane.showOptionDialog(null, "Selecione a conta: ", "Contas", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoesConta, null);
+    
+                        int num_Conta = opcoes.get(contaSelecionada);
+    
+                        // System.out.println(num_Conta);
 
+                        // Percorrendo contas do cliente
+                        for(Conta conta : cliente.getContas()){
+                            // Autenticando conta
+                            if(num_Conta == conta.getNumeroConta()){
+                                // System.out.println("Hello");
 
-                    String numConta = JOptionPane.showInputDialog("Digite o número da conta: ");
-                    Integer autConta = Integer.parseInt(numConta);
+                                List<Integer> menu_interativo = new ArrayList<>();
+                                menu_interativo.add(1);
+                                menu_interativo.add(2);
+                                menu_interativo.add(3);
+                                menu_interativo.add(4);
+                                menu_interativo.add(5);
+                                menu_interativo.add(6);
 
-                    if(autConta == clientes.get(0).getConta().getNumeroConta()){
+                                int menuSelecionado = 1;
+                                while (menu_interativo.get(menuSelecionado) != 6) {
+                                    menuSelecionado = verMenu_principal(menu_interativo);
+                                    if (menu_interativo.get(menuSelecionado) == 1) {
+                                        // Passando o CPF e o número da conta do cliente
+                                        verSaldo(userCPF, num_Conta);
+                                    } else if (menu_interativo.get(menuSelecionado) == 2) {
+                                        depositar();
+                                    } else if (menu_interativo.get(menuSelecionado) == 3) {
+                                        sacar();
+                                    } else if (menu_interativo.get(menuSelecionado) == 4) {
+                                        trasferir();
+                                    } else if(menu_interativo.get(menuSelecionado) == 5){
+                                        verPerfil();
+                                    }
 
-                        List<Integer> menu_interativo = new ArrayList<>();
-                        menu_interativo.add(1);
-                        menu_interativo.add(2);
-                        menu_interativo.add(3);
-                        menu_interativo.add(4);
-                        menu_interativo.add(5);
-                        menu_interativo.add(6);
-
-                        int menuSelecionado = 1;
-                        while (menu_interativo.get(menuSelecionado) != 6) {
-                            menuSelecionado = verMenu_principal(menu_interativo);
-                            if (menu_interativo.get(menuSelecionado) == 1) {
-                                verSaldo();
-                            } else if (menu_interativo.get(menuSelecionado) == 2) {
-                                depositar();
-                            } else if (menu_interativo.get(menuSelecionado) == 3) {
-                                sacar();
-                            } else if (menu_interativo.get(menuSelecionado) == 4) {
-                                trasferir();
-                            } else if(menu_interativo.get(menuSelecionado) == 5){
-                                verPerfil();
+                                }                               
                             }
-
                         }
-
                     }
                 }
             }
+
         }else{
             JOptionPane.showMessageDialog(null, "Nenhum cliente cadastrado!");
         }
@@ -160,16 +159,6 @@ public class App {
         /*UIManager.put("OptionPane.cancelButtonText", "Cancelar"); 
         UIManager.put("OptionPane.noButtonText", "Não"); 
         UIManager.put("OptionPane.yesButtonText", "Sim");*/
-
-
-
-        /* JOptionPane.showMessageDialog(null,
-        "Cliente: " + clientes.get(0).getNome() + "\nAgência: " + clientes.get(0).getConta().getNumeroAgencia() + "\nConta: " + clientes.get(0).getConta().getNumeroConta() + "\nSaldo: " + clientes.get(0).getConta().getSaldo()); */
-        
-        //System.out.println("Cliente: " + clientes.get(0).getNome() + ", Agência: " + clientes.get(0).getConta().getNumeroAgencia() + ", Conta: " + clientes.get(0).getConta().getNumeroConta() + ", Saldo: " + clientes.get(0).getConta().getSaldo());
-
-
-        //System.out.println("Nenhum cliente cadastrado!");
     
 
                 
@@ -185,23 +174,21 @@ public class App {
 
     }
 
-    public static void verSaldo() {
-        List<Integer> saldo_menu = new ArrayList();
-        saldo_menu.add(1);
-        saldo_menu.add(2);
-        Object[] saldoArray = saldo_menu.toArray();
-
-        int opcaoSelecionad = JOptionPane.showOptionDialog(null,
-                "1. Depositar    2. Tranferir" + "\n\nSaldo: " + clientes.get(0).getConta().getSaldo(),
-                "Saldo",
-                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null,
-                saldoArray, null);
+    public static void verSaldo(String CPF, int numConta) {
         
-        if(opcaoSelecionad == 0){
-            depositar();
-        }else if(opcaoSelecionad == 1){
-            trasferir();
+        for(Cliente cliente : clientes){
+            // Autenticando o cliente que será acessado
+            if(CPF.equals(cliente.getCPF())){
+
+                for(Conta conta : cliente.getContas()){
+                    //Autenticando a conta que será acessada
+                    if(numConta == conta.getNumeroConta()){
+                        JOptionPane.showMessageDialog(null, "Cliente: " + cliente.getNome() + "\nSaldo: " + conta.getSaldo());
+                    }
+                }
+            }
         }
+        
     }
 
     public static void depositar() {
